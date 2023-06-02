a142317886f23b6aa42dd2963d72eb1399d51f16
DiuDiuXiong <WujiZhu1999@canva.com>
2023-06-02 21:48:02 +1000

    Include chromedp as dependency

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -3,10 +3,3 @@ package crawler
 type Crawler interface {
 	FetchOnePage(url string) (string, error)
 }
-
-type BasicCrawler struct {
-}
-
-func (bc *BasicCrawler) FetchOnePage(url string) (string, error) {
-	return "", nil
-}
--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -3,6 +3,3 @@ package fetcher
 type Fether interface {
 	FetchContents(jobTitle string, criteria map[string]interface{}) []string
 }
-
-type BasicFetcher struct {
-}
