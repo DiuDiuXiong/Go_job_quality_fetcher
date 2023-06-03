2feed4cf9fa0e4e67506014e195791377c68ae32
DiuDiuXiong <WujiZhu1999@canva.com>
2023-06-04 04:48:54 +1000

    clean un-used type

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,6 @@ import (
 	"fmt"
 )
 
-type windowSize struct {
-	Width  int `json:"width"`
-	Height int `json:"height"`
-}
-
 func main() {
 	frc := crawler.NewFullResponseCrawler()
 	fmt.Print(frc.FetchOnePage("https://uk.indeed.com/jobs?q=software+engineer"))
