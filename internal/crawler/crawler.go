f03f4805f089f0fa35074fec5162309979fc1526
DiuDiuXiong <WujiZhu1999@canva.com>
2023-06-19 13:25:38 +1000

    support multiple fetch

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -7,6 +7,7 @@ import (
 
 type Crawler interface {
 	FetchOnePage(url string) (string, error)
+	FetchManyPages(urls []string) ([]string, error)
 }
 
 func crawWithAction(ctx context.Context, url string, extractAction *chromedp.Action) error {
--- internal/crawler/text_crawler.go
+++ internal/crawler/text_crawler.go
@@ -27,3 +27,20 @@ func (cl *TextCrawler) FetchOnePage(url string) (string, error) {
 	}
 	return res, nil
 }
+
+func (cl *TextCrawler) FetchManyPages(urls []string) ([]string, error) {
+	ctx, cancel := cl.chromeExec.NewContext()
+	defer cancel()
+	ctx, cancel = context.WithTimeout(ctx, time.Duration(int(cl.timeout)*len(urls)))
+	defer cancel()
+	res := make([]string, len(urls))
+
+	for idx, url := range urls {
+		var action chromedp.Action = chromedp.Text("html", &res[idx], chromedp.ByQuery)
+		if err := crawWithAction(ctx, url, &action); err != nil {
+			return nil, err
+		}
+	}
+
+	return res, nil
+}
